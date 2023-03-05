5a8519a442e72a70982fc04581d4df52f4455761
Arsham Jamshidi <arshamjamshidi2022@gmail.com>
2023-03-06 00:52:36 +0330

    Change tailwind configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,26 @@
 import "@/styles/globals.css";
 
-import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "@/utility/createEmotionCache";
+// import { CacheProvider } from "@emotion/react";
+// import createEmotionCache from "@/utility/createEmotionCache";
 
 import { Container } from "@mui/material";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import ScrollButton from "@/components/modules/scrollButton";
 
-const clientSideEmotionCache = createEmotionCache();
+// const clientSideEmotionCache = createEmotionCache();
 
 export default function App({ Component, pageProps }) {
-  const emotionCache = clientSideEmotionCache;
+  // const emotionCache = clientSideEmotionCache;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Container maxWidth="lg">
-        <Header />
-        <ScrollButton />
-        <Component {...pageProps} />
-        <Footer />
-      </Container>
-    </CacheProvider>
+    // <CacheProvider value={emotionCache}>
+    <Container maxWidth="lg">
+      <Header />
+      <ScrollButton />
+      <Component {...pageProps} />
+      <Footer />
+    </Container>
+    // </CacheProvider>
   );
 }
